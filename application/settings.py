84fcded0aa53205977f79badef471130434bca37
b <sedthawut2000@gmail.com>
2020-05-28 21:43:52 +0700

    connect with docker database

--- application/settings.py
+++ application/settings.py
@@ -72,14 +72,24 @@ WSGI_APPLICATION = 'application.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'database',
+        'USER': 'admin',
+        'PASSWORD': 'passwordfordatabase',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
